caef20fe39797988b94faf1ade9fa57f94932bf0
Panupan Tantiwacharapan <ball085573@hotmail.com>
2020-12-07 13:56:21 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function greet(name){
+  return "Hello, i'm " + name;
+}
+
+console.log(greet("panupan"));
+
+
+function makeaAdjectifier(adjective){
+  return function (string){
+    return adjective + " " + string;
+  };
+}
+
+var coolifier = makeaAdjectifier("cool");
+console.log(coolifier("congerence"));
